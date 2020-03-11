da40db1ef1eba904588a3ff0be6ef8a473e6292f
zachalexander <alexander.d.zachary@gmail.com>
2020-03-11 16:20:33 -0400

    adds allowed hosts

--- portfoliobackend/settings.py
+++ portfoliobackend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['guarded-anchorage-28885.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
