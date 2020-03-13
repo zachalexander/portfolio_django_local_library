54c9395aba9735900c6a04a396e18431dd326d98
zachalexander <alexander.d.zachary@gmail.com>
2020-03-13 10:39:39 -0400

    fixes char length in model

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 
 class Users(models.Model):
-    first_name      = models.CharField(max_length=200)
-    last_name       = models.CharField(max_length=200)
-    email           = models.CharField(max_length=200)
+    first_name      = models.CharField(max_length=1000)
+    last_name       = models.CharField(max_length=1000)
+    email           = models.CharField(max_length=1000)
 
 class Tweets(models.Model):
     id             = models.BigIntegerField(primary_key=True)
-    tweetText      = models.CharField(max_length=200, default='NA', null=True)
-    user           = models.CharField(max_length=200, default='NA', null=True)
+    tweetText      = models.CharField(max_length=1000, default='NA', null=True)
+    user           = models.CharField(max_length=1000, default='NA', null=True)
     followers      = models.IntegerField(default='NA', null=True)
-    date           = models.DateTimeField(max_length=200, default='NA', null=True)
-    location       = models.CharField(max_length=200, default='NA', null=True)
-    coordinates_lat= models.CharField(max_length=200, default='NA', null=True)
-    coordinates_lon= models.CharField(max_length=200, default='NA', null=True)
+    date           = models.DateTimeField(max_length=1000, default='NA', null=True)
+    location       = models.CharField(max_length=1000, default='NA', null=True)
+    coordinates_lat= models.CharField(max_length=1000, default='NA', null=True)
+    coordinates_lon= models.CharField(max_length=1000, default='NA', null=True)
 
 class TweetsCount(models.Model):
     count          = models.IntegerField(default='NA', null=True)
